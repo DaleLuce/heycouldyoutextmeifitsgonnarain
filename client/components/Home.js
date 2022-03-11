584b3e379a9cb13f44e01b27627254cc217e7e43
Dale Luce <daleluce87@gmail.com>
2022-03-11 12:10:25 -0500

    more styling on homepage

--- client/components/Home.js
+++ client/components/Home.js
@@ -12,31 +12,29 @@ export const Home = (props) => {
     <section>
       <Clouds />
       <div id="mainContainer">
-        <div className="mainContentHalf">
-          <h4 className="left">What...like...everytime it's gonna rain?</h4>
-          <h4 className="right">
-            Yeah. And also like...when i'm getting ready for work in the
-            morning?
-          </h4>
-          <h4 className="left">
-            Can't you just check on a weather app or something?
-          </h4>
-          <h4 className="right">
-            Yea but..Im always running late in the mornings...
-          </h4>
-        </div>
-
-        <div className="mainContentHalf">
-          <h4 id="left">This seems a little ridiculous, but I guess so....</h4>
-          <h4>
-            Wait....whats your name again? Do I even have your phone number?
-          </h4>
-
+        <div className="mainContent">
+          <div id="mainText">
+            <h4 id="1" className="left">
+              What...like...everytime it's gonna rain?
+            </h4>
+            <h4 className="right">
+              Yeah. And also like...when i'm getting ready for work in the
+              morning?
+            </h4>
+            <h4 className="left">
+              Can't you just check on a weather app or something?
+            </h4>
+            <h4 className="right">Yea but...sometimes I forget.</h4>
+            <h4 id="left">
+              This seems a little ridiculous, but I guess so....
+            </h4>
+          </div>
           <form>
-            <label htmlFor="name">Name:</label>
+            <label htmlFor="name">Name</label>
             <input size="50" type="text" id="name" name="name"></input>
             <label htmlFor="number">Phone Number</label>
             <input size="50" type="text" id="number" name="number"></input>
+            <br></br>
             <button type="submit" value="submit">
               Submit
             </button>
